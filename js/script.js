5324f4c396b1434ea7f43931986c08fd5bb78058
Boyd Ndonga <boydbugle@gmail.com>
2018-07-12 12:29:06 +0300

    restructured code and added comments to make it easy to follow up on the code

--- js/script.js
+++ js/script.js
@@ -1,25 +1,30 @@
 
 
 
-
-
+// initializing jquery
 $(document).ready(function(){
+    // the form submit function event listener
     $("form").submit(function(event){
+        // preventing the browser from reloading after submit button is pressed
         event.preventDefault();
+        // declaring a variable that stores the input that is received from the user
         var noString = $("input#input1").val();
+        // declaring a variable that stores the array that has been split at every plus(+) sign
         var Addition = noString.split('+');
+        // declaring a variable that stores the number that has been added
         var sum = 0;
-        for (var number=0;number<=Addition.length-1;number++) {
-            sum += parseInt(Addition[number]);
-        }
+        // the add function that sums up numbers that have been input by the user
+        var add = function(Addition){
+            // a function that loops through the array created and adds the number into a variable number
+            for (var number=0;number<=Addition.length-1;number++) {
+                sum += parseInt(Addition[number]);
+            }
         console.log(sum);
-            // var sum = 0;
-            // var add = noArray.map( num =>{
-            //     return sum += num
-            // })
-            // alert(sum);
+    }        
+    add(Addition)
     });
 });
+
 
 
 
