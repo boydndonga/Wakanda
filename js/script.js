00429067b292486e19c3d4f5ffc24682f878a712
Boyd Ndonga <boydbugle@gmail.com>
2018-07-12 12:14:53 +0300

    created addition function and made changes to the index file to allow numbers to be added using the addition sign

--- js/script.js
+++ js/script.js
@@ -1,8 +1,45 @@
+
+
+
+
+
 $(document).ready(function(){
     $("form").submit(function(event){
         event.preventDefault();
         var noString = $("input#input1").val();
-        var noArray = parseInt(noString.split(','));
-        alert(noArray[1]);
+        var Addition = noString.split('+');
+        var sum = 0;
+        for (var number=0;number<=Addition.length-1;number++) {
+            sum += parseInt(Addition[number]);
+        }
+        console.log(sum);
+            // var sum = 0;
+            // var add = noArray.map( num =>{
+            //     return sum += num
+            // })
+            // alert(sum);
     });
 });
+
+
+
+
+
+
+
+
+
+// $(document).ready(function(){
+//     $("form").submit(function(event){
+//       event.preventDefault();
+//       var noString = $("input#userInput").val();
+//       var numArray = noString.split("+");
+//       var sum = 0;
+  
+//       for (var x=0;x<=numArray.length-1;x++){
+//         sum += parseInt(numArray[x]);
+//       }
+//       console.log(sum);
+//     });
+  
+//   });
